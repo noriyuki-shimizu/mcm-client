097a703033f54ec2f6c9fe22e53f8d8c5748909b
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-03-09 00:41:11 +0900

    uikitを適応

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,17 @@
   </div>
 </template>
 
+<script>
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+UIkit.use(Icons);
+
+export default {
+  name: 'App',
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import url('../node_modules/uikit/dist/css/uikit.css');
 </style>
