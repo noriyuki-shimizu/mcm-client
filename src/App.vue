46f0d3f6338adf8b7c6c1c5bbd540c31d38b5503
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-03-10 23:34:46 +0900

    サイドメニューを追加

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <side-bar/>
     </div>
     <router-view/>
   </div>
@@ -11,12 +10,17 @@
 <script>
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+import SideBar from '@/components/SideBar.vue';
+import { Component, Vue } from 'vue-property-decorator';
 
 UIkit.use(Icons);
+
+Vue.component('side-bar', SideBar);
 
 export default {
   name: 'App',
 };
+
 </script>
 
 <style lang="scss">
---
+++ src/components/SideBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="side-bar">
+    <span class="uk-margin-auto-right" uk-icon="menu" uk-toggle="target: #offcanvas-push">Menu</span>
+
+    <div id="offcanvas-push" uk-offcanvas="mode: push; overlay: true">
+        <div class="uk-offcanvas-bar">
+
+          <div class="uk-width-1-2@s uk-width-2-5@m">
+            <ul class="uk-nav-default uk-nav-parent-icon" uk-nav>
+              <li>
+                <router-link to="/">Home</router-link>
+              </li>
+              <li>
+                <router-link to="/about">About</router-link>
+              </li>
+              <li class="uk-parent">
+                  <a href="#">My Clothes</a>
+                  <ul class="uk-nav-sub">
+                      <li><a href="#">Sub item</a></li>
+                      <li>
+                          <a href="#">Sub item</a>
+                          <ul>
+                              <li><a href="#">Sub item</a></li>
+                              <li><a href="#">Sub item</a></li>
+                          </ul>
+                      </li>
+                  </ul>
+              </li>
+              <li class="uk-parent">
+                  <a href="#">Parent</a>
+                  <ul class="uk-nav-sub">
+                      <li><a href="#">Sub item</a></li>
+                      <li><a href="#">Sub item</a></li>
+                  </ul>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+    </div>
+  </div>
+
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'side-bar',
+};
+
+</script>
