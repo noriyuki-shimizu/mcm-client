b973b40bc41a9e3e68f034d30caefed9d2a7c351
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-03-14 23:12:59 +0900

    ホーム画面用のトップコンポーネントを追加

---
+++ src/components/home/HomeTop.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="uk-section uk-section-muted">
+        <div class="uk-panel uk-margin-xlarge-left">
+            <img class="uk-align-left uk-margin-remove-adjacent" src="https://66.media.tumblr.com/088b5a735bb59f939eea37b507765914/tumblr_nbjjpgDciI1qij5oho1_1280.jpg" width="600"> 
+            <h1>My clothes management tool.</h1>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HomeTop extends Vue {
+}
+</script>
