8c210ecf12b9db4ac7a92ace98977deb36deeffc
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-03-15 23:15:22 +0900

    ホーム画面用のトップコンポーネント完成

--- src/components/home/HomeTop.vue
+++ src/components/home/HomeTop.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="uk-section uk-section-muted">
-        <div class="uk-panel uk-margin-xlarge-left">
-            <img class="uk-align-left uk-margin-remove-adjacent" src="https://66.media.tumblr.com/088b5a735bb59f939eea37b507765914/tumblr_nbjjpgDciI1qij5oho1_1280.jpg" width="600"> 
-            <h1>My clothes management tool.</h1>
+    <div class="home-top">
+        <div class="uk-section uk-section-muted">
+            <div class="uk-panel uk-margin-xlarge-left">
+                <img class="uk-align-left uk-margin-remove-adjacent" src="../../images/home/home.jpg" width="600"> 
+                <h1>My clothes management tool.</h1>
+            </div>
         </div>
     </div>
 </template>
