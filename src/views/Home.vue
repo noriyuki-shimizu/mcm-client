c2740ec2dd9b0ff510897e230611eb1d9bd33280
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-03-17 17:49:12 +0900

    メニューの階層を示すvue componentを読み込むよう修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <breadcrumb :hierarchyList="hierarchyList"></breadcrumb>
+
     <home-top />
 
     <hr class="uk-divider-icon">
@@ -15,19 +17,21 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import Breadcrumb from '@/components/breadcrumb/Breadcrumb.vue';
 import HomeTop from '@/components/home/HomeTop.vue';
 import HomeBrand from '@/components/brand/HomeBrand.vue';
 import HomeShop from '@/components/shop/HomeShop.vue';
 
 @Component({
   components: {
+    Breadcrumb,
     HomeBrand,
     HomeTop,
     HomeShop,
   },
 })
-
 export default class Home extends Vue {
+  private hierarchyList: string[] = ['Home'];
 }
 
 </script>
