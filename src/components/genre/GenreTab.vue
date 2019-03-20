8cc216b874d7142a923a854d04e86634a74aa151
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-03-20 23:16:01 +0900

    ジャンルにおけるタブや合計金額のコンポーネントを作成

---
+++ src/components/genre/GenreTab.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="genre-tab">
+        <ul class="uk-child-width-expand" uk-tab>
+            <li v-for="(genre, index) in genreList" :key="(genre, index)" :class="{ 'uk-active': isFirst(index) }">
+                <a href="#">{{ genre }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+type CustomProp<T> = () => T;
+
+@Component
+export default class GenreTab extends Vue {
+    @Prop({type: Array as CustomProp<string[]>})
+    private genreList!: string[];
+
+    private isFirst(index: number): boolean {
+        return index === 0;
+    }
+}
+</script>
---
+++ src/components/genre/TotalPricePerGenreList.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="total-price-per-genre-list">
+        <div class="uk-section uk-section-default">
+            <div class="uk-container uk-margin-medium">
+                <h3>Total price</h3>
+                <div v-for="(totalPricePerGenre, index) in list" :key="(totalPricePerGenre, index)" class="uk-grid-small" uk-grid>
+                    <div class="uk-width-expand" uk-leader>{{ totalPricePerGenre.genre }}</div>
+                    <div>{{ totalPricePerGenre.totalPrice | priceFormat }}</div>
+                </div>
+                <hr>
+                <b>
+                    <div class="uk-grid-small" uk-grid>
+                        <div class="uk-width-expand" uk-leader>Total</div>
+                        <div>{{ getTotalPrice() | priceFormat }}</div>
+                    </div>
+                </b>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue} from 'vue-property-decorator';
+
+type CustomProp<T> = () => T;
+
+@Component({
+    filters: {
+        priceFormat(price: number): string {
+            return '¥ ' + price.toLocaleString();
+        },
+    },
+})
+export default class TotalPricePerGenreList extends Vue {
+    @Prop({type: Array as CustomProp<any[]>})
+    private list!: any[];
+
+    private getTotalPrice(): number {
+        return this.list.map((totalPricePerGenre, index) => {
+            return totalPricePerGenre.totalPrice;
+        }).reduce((previous, current, index, array) => {
+            return previous + current;
+        });
+    }
+}
+</script>
