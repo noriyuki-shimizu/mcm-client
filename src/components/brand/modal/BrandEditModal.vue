41495fcbc06e1ac292e6934ff81690a67a742bcd
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-03-25 21:19:55 +0900

    ブランド編集でのモーダルコンポーネントを追加

---
+++ src/components/brand/modal/BrandEditModal.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="brand_edit_modal" class="uk-modal-container" uk-modal>
+        <div class="uk-modal-dialog uk-modal-body">
+            <button class="uk-modal-close-default" type="button" uk-close></button>
+            <h2 class="uk-modal-title">Headline</h2>
+
+            <brand-edit-form />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import BrandEditForm from '@/components/brand/form/edit/BrandEditForm.vue';
+
+type CustomProp<T> = () => T;
+type BrandDto = {
+    name: string,
+    link: string,
+    imagePath: string,
+    address: string,
+    businessHours: string,
+    tel: string,
+    deleteFlag: boolean,
+};
+
+@Component({
+    components: {
+        BrandEditForm,
+    },
+})
+export default class BrandEditModal extends Vue {
+    @Prop({type: Object as CustomProp<BrandDto>})
+    private brandDto!: BrandDto;
+}
+</script>
