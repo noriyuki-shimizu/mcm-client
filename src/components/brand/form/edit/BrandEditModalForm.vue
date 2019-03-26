37b018ce030677e8ccef0f000d2e9d0c008636de
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-03-26 23:38:24 +0900

    モーダルとフォームを分けてコンポーネント化していたが、ネストが深くなるため、統一

--- src/components/brand/form/edit/BrandEditModalForm.vue
+++ src/components/brand/form/edit/BrandEditModalForm.vue
@@ -3,15 +3,13 @@
         <div class="uk-modal-dialog uk-modal-body">
             <button class="uk-modal-close-default" type="button" uk-close></button>
             <h2 class="uk-modal-title">Headline</h2>
-
-            <brand-edit-form />
+            
         </div>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import BrandEditForm from '@/components/brand/form/edit/BrandEditForm.vue';
 
 type CustomProp<T> = () => T;
 type BrandDto = {
@@ -24,13 +22,9 @@ type BrandDto = {
     deleteFlag: boolean,
 };
 
-@Component({
-    components: {
-        BrandEditForm,
-    },
-})
+@Component
 export default class BrandEditModal extends Vue {
     @Prop({type: Object as CustomProp<BrandDto>})
-    private brandDto!: BrandDto;
+    private brandDto?: BrandDto;
 }
 </script>
