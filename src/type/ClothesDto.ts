59ab4ee5aae5455fe9f7a02341c860e7bc777487
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-04-03 09:26:42 +0900

    服関連のDTOを追加

---
+++ src/type/ClothesDto.ts
@@ -0,0 +1,10 @@
+export default class ClothesDto {
+    public id!: number;
+    public imagePath?: string;
+    public genre?: string;
+    public brand?: string;
+    public shop?: string;
+    public price?: number;
+    public buyDate?: string;
+    public deleteFlg?: boolean;
+}
