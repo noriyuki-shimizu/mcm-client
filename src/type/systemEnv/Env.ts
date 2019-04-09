0da02542765c5fc92781882c978451ce6f8a8b17
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-04-10 08:53:41 +0900

    yamlファイルの読み込みオブジェクトの追加

---
+++ src/type/systemEnv/Env.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs';
+import * as yaml from 'yaml';
+
+interface ServerApp {
+    [key: string]: string;
+    local: string;
+}
+
+/**
+ * 設定ファイル読み込みに関するクラス。
+ *
+ * @export
+ * @class Env
+ */
+export class Env {
+
+	private yamlPropertys: ServerApp;
+
+	constructor() {
+		const appYml: string = fs.readFileSync('@/config/application.yaml', 'utf8');
+
+        this.yamlPropertys = yaml.parse(appYml);	
+	}
+	protected load(key: string): any {
+		const filler:string = '.';
+		
+		if(key.indexOf(filler) >= 0) {
+            var keyList:string[]  = key.split(filler);
+
+            return this.scan(this.yamlPropertys, keyList.values());
+        }
+
+        return this.yamlPropertys[key];
+	}
+	private scan(yamlProperty: any, keyIterator: any): any {
+		var next: {value: number; done: boolean} = keyIterator.next();
+
+		if(next.done) {
+			return yamlProperty;
+		}
+
+		return this.scan(yamlProperty[next.value], keyIterator);	
+	}
+}
