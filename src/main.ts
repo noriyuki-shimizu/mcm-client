1e28883d854e6f77b8c605989408d38a083d7bc1
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-04-09 11:13:41 +0900

    firebaseの設定情報を追加

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import Vue from 'vue';
+import firebase from 'firebase/app';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+const config = {
+  apiKey: 'AIzaSyCSAxcqVOgM_XdTIJTOv4mckREJEcnivsc',
+  authDomain: 'mcm-client.firebaseapp.com',
+  databaseURL: 'https://mcm-client.firebaseio.com',
+  projectId: 'mcm-client',
+  storageBucket: 'mcm-client.appspot.com',
+  messagingSenderId: '1013848755479',
+};
+firebase.initializeApp(config);
 
 Vue.config.productionTip = false;
 
