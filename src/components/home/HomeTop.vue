e1a56bcb18bd0bce078e13de289c0f0034901817
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-05-06 23:24:19 +0900

    ホーム画面の見出しを修正

--- src/components/home/HomeTop.vue
+++ src/components/home/HomeTop.vue
@@ -3,7 +3,7 @@
         <div class="uk-section uk-section-muted">
             <div class="uk-panel uk-margin-xlarge-left">
                 <img class="uk-align-left uk-margin-remove-adjacent" src="../../images/home/home.jpg" width="600"> 
-                <h1>My clothes management tool.</h1>
+                <h1>My clothes.</h1>
             </div>
         </div>
     </div>
