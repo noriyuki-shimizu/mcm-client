0ba0fb6d24e3eb89a865b127f179cdcde65e7c08
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-05-07 22:27:56 +0900

    テーブル用の服データを追加

---
+++ src/type/domain/dto/myClothes/ClothesTableData.ts
@@ -0,0 +1,10 @@
+export default interface ClothesTableData {
+    id: number;
+    imagePath: string;
+    genreName: string;
+    brandName: string;
+    shopName: string;
+    price: number;
+    buyDate: Date;
+    deleteFlag: boolean;
+}
