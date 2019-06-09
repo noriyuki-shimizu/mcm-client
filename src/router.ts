ce7fa838ee79886c2369a15ec196a612bde65936
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-06-09 21:41:24 +0900

    import の記述方法を変更

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-
-import Login from './views/Login.vue';
-import Home from './views/Home.vue';
-import LeafletMap from './views/LeafletMap.vue';
-import ClothesList from './views/myClothes/ClothesList.vue';
-import TotalPrice from './views/myClothes/TotalPrice.vue';
-import ShopMaintenance from './views/maintenance/ShopMaintenance.vue';
-import BrandMaintenance from './views/maintenance/BrandMaintenance.vue';
-import ClothesMaintenance from './views/maintenance/ClothesMaintenance.vue';
-import firebase from '@/firebase/auth';
 import store from '@/store';
 
 Vue.use(Router);
@@ -21,52 +11,65 @@ const router: Router =  new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'login',
-      component: Login,
+      path: '/signin',
+      name: 'signin',
+      component: () => import('./views/Signin.vue'),
       meta: { requiresAuth: false },
     },
     {
-      path: '/home',
-      name: 'home',
-      component: Home,
-      meta: { requiresAuth: true },
+      path: '/signup',
+      name: 'signup',
+      component: () => import('./views/Signup.vue'),
+      meta: { requiresAuth: false },
     },
     {
-      path: '/map',
-      name: 'leaflet-map',
-      component: LeafletMap,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/clothesList',
-      name: 'clothes-list',
-      component: ClothesList,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/totalPrice',
-      name: 'total-price',
-      component: TotalPrice,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/shopMaintenance',
-      name: 'shop-maintenance',
-      component: ShopMaintenance,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/brandMaintenance',
-      name: 'brand-maintenance',
-      component: BrandMaintenance,
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/clothesMaintenance',
-      name: 'clothes-maintenance',
-      component: ClothesMaintenance,
-      meta: { requiresAuth: true },
+      path: '/',
+      component: () => import('./views/Common.vue'),
+      meta: { requiresAuth: false },
+      children: [
+        {
+          path: '/home',
+          name: 'home',
+          component: () => import('./views/Home.vue'),
+          meta: { requiresAuth: true },
+        },
+        {
+          path: '/map',
+          name: 'leaflet-map',
+          component: () => import('./views/LeafletMap.vue'),
+          meta: { requiresAuth: true },
+        },
+        {
+          path: '/clothesList',
+          name: 'clothes-list',
+          component: () => import('./views/myClothes/ClothesList.vue'),
+          meta: { requiresAuth: true },
+        },
+        {
+          path: '/totalPrice',
+          name: 'total-price',
+          component: () => import('./views/myClothes/TotalPrice.vue'),
+          meta: { requiresAuth: true },
+        },
+        {
+          path: '/shopMaintenance',
+          name: 'shop-maintenance',
+          component: () => import('./views/maintenance/ShopMaintenance.vue'),
+          meta: { requiresAuth: true },
+        },
+        {
+          path: '/brandMaintenance',
+          name: 'brand-maintenance',
+          component: () => import('./views/maintenance/BrandMaintenance.vue'),
+          meta: { requiresAuth: true },
+        },
+        {
+          path: '/clothesMaintenance',
+          name: 'clothes-maintenance',
+          component: () => import('./views/maintenance/ClothesMaintenance.vue'),
+          meta: { requiresAuth: true },
+        },
+      ],
     },
   ],
 });
@@ -76,7 +79,7 @@ router.beforeEach((to, from, next) => {
     // Already authenticated.
     const signedIn = store.getters[`${namespace}/isSignedIn`];
 
-    signedIn ? next() : next({ name: 'login' });
+    signedIn ? next() : next({ name: 'signin' });
 
     return ;
   }
