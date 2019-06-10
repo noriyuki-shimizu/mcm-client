bab9cf23f8d64835f661fe1a42bdc15b1a2cca45
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-06-10 21:32:51 +0900

    全てのコンポーネントの共通処理を行うコンポーネントを追加

---
+++ src/views/Common.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="common">
+        <side-bar/>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script lang="ts">
+import SideBar from '@/components/bar/SideBar.vue';
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+      SideBar,
+  },
+})
+export default class Common extends Vue {
+}
+</script>
