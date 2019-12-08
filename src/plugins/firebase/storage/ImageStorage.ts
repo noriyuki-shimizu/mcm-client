39128f42c9cbd54ba032c7ac99ac4577fef88c32
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-12-08 23:15:39 +0900

    画像アップロード処理の修正

--- src/plugins/firebase/storage/ImageStorage.ts
+++ src/plugins/firebase/storage/ImageStorage.ts
@@ -1,46 +1,46 @@
 import * as Filebase from 'firebase/app';
 
-type CallbackType = (arg: string) => void;
+const getImageRef = (name: string) => {
+    const storageRef = Filebase.storage().ref();
+
+    if (!name) {
+        throw new Error('ImageStorage instance feild.');
+    }
+
+    const IMAGE_FOLDER_NAME = 'images/';
+    return storageRef.child(IMAGE_FOLDER_NAME + name);
+};
+
+const upload = async (name: string, file: File | undefined): Promise<string> => {
+    if (!file) {
+        throw new Error('Necessary information is missing!!');
+    }
+
+    const imageRef = getImageRef(name);
+
+    const snapshot = await imageRef.put(file);
+    const downloadURL: string = await snapshot.ref.getDownloadURL();
+
+    return downloadURL;
+};
+
+const deleteImage = (name: string): void => {
+    const imageRef = getImageRef(name);
+    // Delete the file
+    imageRef
+        .delete()
+        .then(() => {
+            // File deleted successfully
+            console.info('File deleted successfully.');
+        })
+        .catch((error: any) => {
+            // Uh-oh, an error occurred!
+            console.error(error);
+            console.error('Image name is %o', name);
+        });
+};
 
 export default {
-    getImageRef(name: string) {
-        const storageRef = Filebase.storage().ref();
-
-        if (!name) {
-            throw new Error('ImageStorage instance feild.');
-        }
-
-        const IMAGE_FOLDER_NAME = 'images/';
-        return storageRef.child(IMAGE_FOLDER_NAME + name);
-    },
-
-    upload(name: string, file: File | undefined, callback: CallbackType): void {
-        if (!file) {
-            throw new Error('Necessary information is missing!!');
-        }
-
-        const imageRef = this.getImageRef(name);
-
-        imageRef.put(file).then(snapshot => {
-            snapshot.ref.getDownloadURL().then(downloadURL => {
-                callback(downloadURL);
-            });
-        });
-    },
-
-    delete(name: string): void {
-        const imageRef = this.getImageRef(name);
-        // Delete the file
-        imageRef
-            .delete()
-            .then(() => {
-                // File deleted successfully
-                console.info('File deleted successfully.');
-            })
-            .catch((error: any) => {
-                // Uh-oh, an error occurred!
-                console.error(error);
-                console.error('Image name is %o', name);
-            });
-    }
+    upload,
+    deleteImage,
 }
